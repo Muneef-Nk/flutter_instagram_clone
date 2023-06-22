8c9330a83409e2b0c8fef1dc090eb88d13eebaeb
Muhammed Muneef M <md.muneefnk@gmail.com>
2023-06-22 12:37:40 +0530

    image error fixed

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -19,8 +19,8 @@ class PostModel{
 List<PostModel>PostDetails=[
   PostModel(
       name: 'Hugh Byrd',
-      profile: 'https://randomuser.me/api/portraits/men/34.jpg',
-      post: 'https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80',
+      profile: 'https://randomuser.me/api/portraits/men/46.jpg',
+      post: 'https://picsum.photos/200/300?random',
       location: 'Atlanta',
       likes: '278',
     discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -30,7 +30,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Cory Richards',
       profile: 'https://randomuser.me/api/portraits/men/94.jpg',
-      post: 'https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+      post: 'https://picsum.photos/200/300?random=1',
       location: 'US',
       likes: '987',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -39,7 +39,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Armando Miller',
       profile: 'https://randomuser.me/api/portraits/men/82.jpg',
-      post: 'https://picsum.photos/200/300?random=1',
+      post: 'https://picsum.photos/200/300?random=2',
       location: 'Beadlow Priory',
       likes: '765',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -48,7 +48,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Maureen Dean',
       profile: 'https://randomuser.me/api/portraits/women/2.jpg',
-      post: 'https://picsum.photos/200/300?random=2',
+      post: 'https://picsum.photos/200/300?random=3',
       location: 'location',
       likes: '999',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -57,7 +57,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Jerry Brewer',
       profile: 'https://randomuser.me/api/portraits/men/45.jpg',
-      post: 'https://picsum.photos/200/300?random=3',
+      post: 'https://picsum.photos/200/300?random=4',
       location: 'location',
       likes: '3542',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -66,7 +66,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Gloria Wilson',
       profile: 'https://randomuser.me/api/portraits/women/28.jpg',
-      post: 'https://picsum.photos/200/300?random=4',
+      post: 'https://picsum.photos/200/300?random=5',
       location: 'Hyderabad',
       likes: '1K',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -75,7 +75,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Ernest Elliott',
       profile: 'https://randomuser.me/api/portraits/men/91.jpg',
-      post: 'https://picsum.photos/200/300?random=5',
+      post: 'https://picsum.photos/200/300?random=6',
       location: 'San Francisco',
       likes: '5635',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -84,7 +84,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Vivan Steward',
       profile: 'https://randomuser.me/api/portraits/women/83.jpg',
-      post: 'https://picsum.photos/200/300?random=6',
+      post: 'https://picsum.photos/200/300?random=7',
       location: 'Jaipur',
       likes: '63653',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -93,7 +93,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Willie Elliott',
       profile: 'https://randomuser.me/api/portraits/men/73.jpg',
-      post: 'https://picsum.photos/200/300?random=7',
+      post: 'https://picsum.photos/200/300?random=8',
       location: 'Washington',
       likes: '7354',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
@@ -102,7 +102,7 @@ List<PostModel>PostDetails=[
   PostModel(
       name: 'Joann Thompson',
       profile: 'https://randomuser.me/api/portraits/women/66.jpg',
-      post: 'https://picsum.photos/200/300?random=18',
+      post: 'https://picsum.photos/200/300?random=9',
       location: 'Houston',
       likes: '1238',
       discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English'
